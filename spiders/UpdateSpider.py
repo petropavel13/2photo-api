6f8bbe5cb209825c17a29d85e836bffee2f31e56
null_ptr <mail4work0xFF@gmail.com>
2014-05-17 16:24:44 +0600

    fix

--- spiders/UpdateSpider.py
+++ spiders/UpdateSpider.py
@@ -40,30 +40,26 @@ class UpdateSpider(BaseSpider):
             while True:
                 posts = { int(p['id']): p for p in g.go(task.url + ('?limit=10&offset=%d' % max_offset)).json['posts'] }
 
-                # logging.debug('Searching db post id. current: %d; jump_size: %d; count: %d', (max_offset, past_jump, cnt))
-
                 if last_post_id_in_db in posts:
                     break
                 else:
                     max_offset += 10
 
 
-        all_posts = []
+            all_posts = []
 
 
-        # all_posts.extend(g.go('http://www.2photo.ru/ru/post/stream.json?limit=8&offset=190').json['posts'])
+            # all_posts = g.go(task.url + ('?limit=256&offset=4096')).json['posts']
 
-        # all_posts = g.go(task.url + ('?limit=256&offset=4096')).json['posts']
+            for x in range(0, max_offset + 10, 10):
+                logging.info('Loading stream posts %d-%d (%d) time:%d' % (max_offset - x, max_offset - x - 256,  max_offset, g.response.total_time,))
+                all_posts.extend(g.go(task.url + ('?limit=10&offset=%d' % x)).json['posts'])
 
-        for x in range(0, max_offset + 10, 10):
-            logging.info('Loading stream posts %d-%d (%d) time:%d' % (max_offset - x, max_offset - x - 256,  max_offset, g.response.total_time,))
-            all_posts.extend(g.go(task.url + ('?limit=10&offset=%d' % x)).json['posts'])
+            # yield Task('post', url=self.BASE_URL + '/ru/post/32033', post=list(filter(lambda x: x['id'] == '32033', all_posts))[0])
+            # yield Task('post', url=self.BASE_URL + '/ru/post/31279', post=list(filter(lambda x: x['id'] == '31279', all_posts))[0])
 
-        # yield Task('post', url=self.BASE_URL + '/ru/post/32033', post=list(filter(lambda x: x['id'] == '32033', all_posts))[0])
-        # yield Task('post', url=self.BASE_URL + '/ru/post/31279', post=list(filter(lambda x: x['id'] == '31279', all_posts))[0])
+            post_ids_in_db = frozenset(Post.objects.values_list('id', flat=True).all())
 
-        post_ids_in_db = frozenset(Post.objects.values_list('id', flat=True).all())
-
-        for post in all_posts:
-            if int(post['id']) not in post_ids_in_db:
-                yield Task('post', url=self.BASE_URL + post['link'], post=post)
+            for post in all_posts:
+                if int(post['id']) not in post_ids_in_db:
+                    yield Task('post', url=self.BASE_URL + post['link'], post=post)
