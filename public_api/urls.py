a6dc5bcc23af6343e0e5b473e6fc8d24fd7c5694
petropavel13 <mail4work0xFF@gmail.com>
2014-09-10 22:47:28 +0600

    refactor structure

---
+++ public_api/urls.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+
+from django.conf.urls import patterns, include, url
+
+from public_api.rest import TagViewSet, CategoryViewSet, UserViewSet, ArtistViewSet, CommentViewSet, PostViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+router.register(prefix=r'tags', viewset=TagViewSet)
+router.register(prefix=r'categories', viewset=CategoryViewSet)
+router.register(prefix=r'authors', viewset=UserViewSet)
+router.register(prefix=r'artists', viewset=ArtistViewSet)
+router.register(prefix=r'comments', viewset=CommentViewSet)
+router.register(prefix=r'posts', viewset=PostViewSet)
+
+
+urlpatterns = patterns('',
+	url('', include(router.urls)),
+)
