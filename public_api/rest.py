petropavel13/2photo-api

commit e49f123fbbd83b58152d1c4fef9a18556a0c4b92

add queryset attribute to CommentViewSet class

public_api/rest.py
@@ -53,7 +53,8 @@ class CategoryViewSet(ReadOnlyModelViewSet):
 
 
 class CommentViewSet(ReadOnlyModelViewSet):
-    model = Comment
+    queryset = Category.objects.all()
+
     filter_fields = ('author', 'post',)
     ordering = ('-date',)
 

commit 679191075ee3f321bd695f692dbb202bcd4b4b49

fix mistake introduced in e49f123

public_api/rest.py
@@ -54,7 +54,7 @@ class CategoryViewSet(ReadOnlyModelViewSet):
 
 
 class CommentViewSet(ReadOnlyModelViewSet):
-    queryset = Category.objects.all()
+    queryset = Comment.objects.all()
 
     filter_fields = ('author', 'post',)
     ordering = ('-date',)